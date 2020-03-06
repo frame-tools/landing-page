1f90a55dfd2eb9948d8a8efd0277ba7b618712f8
Rodrigo Waltenberg <rodrigo@frametools.dev>
2020-03-06 18:17:01 -0300

    Contact function

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function(eleventyConfig) {
   })
 
   eleventyConfig.addPassthroughCopy("src/assets/**/!(*.scss)")
+  eleventyConfig.addPassthroughCopy("api/**/*")
 
   // date filter (localized)
   eleventyConfig.addFilter("date", function(date, format, locale) {

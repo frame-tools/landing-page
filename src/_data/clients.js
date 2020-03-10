f00b5d61414b532999a261b755bdfc531bc45524
Rodrigo Waltenberg <rodrigo@frametools.dev>
2020-03-10 09:03:00 -0300

    clients and teams data export

---
+++ src/_data/clients.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: 'Grupo Boticário',
+    logo: '/assets/images/clients/boticario.png'
+  },
+  {
+    name: 'Beauty Date',
+    logo: '/assets/images/clients/beautydate.png'
+  },
+  {
+    name: 'Idox - Tecnologias Jurídicas',
+    logo: '/assets/images/clients/idox.png'
+  },
+  {
+    name: 'Mala Pronta',
+    logo: '/assets/images/clients/malapronta.png'
+  },
+  {
+    name: 'Audiowalk Curitiba',
+    logo: '/assets/images/clients/audiowalk.png'
+  },
+]
---
+++ src/_data/team.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    name: 'Ernesto Araújo',
+    image: '/assets/images/avatar-ernesto.png',
+    skills: ['Mobile Engineer / Data Scientist', 'Head of Product']
+  },
+  {
+    name: 'Laís Amaro',
+    image: '/assets/images/avatar-lais.png',
+    skills: ['Administrator', 'Head of Operations']
+  },
+  {
+    name: 'Rodrigo Waltenberg',
+    image: '/assets/images/avatar-rodrigo.png',
+    skills: ['Frontend Engineer', 'Head of Technology']
+  },
+  {
+    name: 'Marcos Oliveira',
+    image: '/assets/images/avatar-marcos.png',
+    skills: ['Backend Engineer', 'Tech Lead']
+  },
+  {
+    name: 'Fabrizzio Zampieri',
+    image: '/assets/images/avatar-zamp.png',
+    skills: ['Designer / UX Specialist']
+  },
+  {
+    name: 'Marlos Irapuan',
+    image: '/assets/images/avatar-marlos.png',
+    skills: ['Fullstack Engineer']
+  },
+  {
+    name: 'Tarcisio Melo',
+    image: '/assets/images/avatar-tarcisio.png',
+    skills: ['Frontend Engineer']
+  }
+]
